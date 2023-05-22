84d66466bbdd1188db20982f666b97d8dad3e836
Nanna Hansen <nann257j@stud.kea.dk>
2023-05-22 13:48:48 +0200

    cookiebox

--- src/main.js
+++ src/main.js
@@ -35,6 +35,22 @@ inView(".cardinview", ({ target }) => {
   );
 });
 
+// ----------- COOKIE BOKS ------------- //
+const acceptCookie = document.querySelector("#accepter");
+const denyCookie = document.querySelector("#afvis");
+const cookieboks = document.querySelector("#cookieboks");
+
+acceptCookie.addEventListener("click", fjernCookieBoks);
+denyCookie.addEventListener("click", fjernCookieBoks);
+
+function fjernCookieBoks() {
+  cookieboks.classList.add("hidden");
+  sessionStorage.setItem("fjerncookieboks", true);
+}
+if (sessionStorage.getItem("fjerncookieboks")) {
+  cookieboks.classList.add("hidden");
+}
+
 // ---------- LOGIN ----------- //
 class Login extends HTMLElement {
   constructor() {
@@ -57,7 +73,7 @@ class Login extends HTMLElement {
             max-width: 350px;
             width: 100% !important;
             background-color: #F7F7F7;
-            margin: 350px auto;
+            margin: 0 auto;
             border-radius: 2px;
             box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
             overflow: hidden;
