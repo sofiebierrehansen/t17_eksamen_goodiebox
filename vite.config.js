5da2ddc1825987b19d20453733a3d875acb889b0
Sofie Bierre Hansen <sofi02a7@stud.kea.dk>
2023-05-30 11:58:10 +0200

    klargøring

--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,13 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        beautyprofil: resolve(__dirname, "src/beautyprofil.html"),
+        fordele: resolve(__dirname, "src/minefordele.html"),
+        abonnement: resolve(__dirname, "src/mitabonnement.html"),
+        personligeoplysninger: resolve(__dirname, "src/personligeoplys.html"),
+        singleview: resolve(__dirname, "src/singleview.html"),
+        sitemap: resolve(__dirname, "src/sitemap.html"),
+        tilpasbox: resolve(__dirname, "src/tilpasbox.html"),
         // about: resolve(__dirname, "src/about.html"),
       },
     },
